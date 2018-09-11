abac37528b9d357293dc5b1a4f99717a3e20b649
babylee2002 <babylee2002@gmail.com>
2018-09-11 09:06:28 -0400

    creating components

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
+    state = {
+        count: 0,
+    };
+
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <span>{this.state.count}</span>
                 <button>Increment</button>
             </div>);
 
