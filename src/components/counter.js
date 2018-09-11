fb26956d5ecd57a2264a4eb22ed807ebe8511e80
babylee2002 <babylee2002@gmail.com>
2018-09-11 10:05:31 -0400

    counter update

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,15 +4,37 @@ class Counter extends Component {
 
     state = {
         count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+
     };
+    styles = {
+        fontSize: 50,
+        fontWeight: 'bold'
+    }
 
     render() {
+
         return (
             <div>
-                <span>{this.state.count}</span>
-                <button>Increment</button>
-            </div>);
+                <span
+                    style={this.styles}
+                    className={this.getBadgeClasses()}>{this.state.count}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
 
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>
+                        {tag}
+                    </li>)}
+                </ul>
+            </div>
+        );
+
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 }
 
